65ab4b2a59c970b088104c351d9433b93a44b907
mazerty <mazerty@gmail.com>
2018-08-22 14:43:24 +0000

    some cleaning of code we won't need in the demo

--- src/main/java/org/zebr0/demovaadin8/cdi/event/UserChangedEvent.java
+++
@@ -1,7 +0,0 @@
-package org.zebr0.demovaadin8.cdi.event;
-
-/**
- * Event fired when a change happened to one or more users
- */
-public class UserChangedEvent {
-}
--- src/main/java/org/zebr0/demovaadin8/vaadin/DemoVaadin8UI.java
+++ src/main/java/org/zebr0/demovaadin8/vaadin/DemoVaadin8UI.java
@@ -4,15 +4,13 @@ import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.cdi.CDIUI;
-import com.vaadin.cdi.CDIViewProvider;
-import com.vaadin.navigator.Navigator;
-import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.Window;
-import org.zebr0.demovaadin8.cdi.Session;
-
-import javax.inject.Inject;
+import org.vaadin.viritin.button.MButton;
+import org.vaadin.viritin.label.MLabel;
+import org.vaadin.viritin.layouts.MPanel;
+import org.vaadin.viritin.layouts.MVerticalLayout;
 
 @CDIUI("")
 @Theme("valo")
@@ -20,54 +18,16 @@ import javax.inject.Inject;
 @Title("Vaadin 8 Demo")
 public class DemoVaadin8UI extends UI {
 
-    @Inject
-    private CDIViewProvider cdiViewProvider;
-    @Inject
-    private Session session;
-
-    private Navigator navigator;
-
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        // defines that the application consists of a set of annotated views you can access through the ui's navigator
-        navigator = new Navigator(this, this);
-        navigator.addProvider(cdiViewProvider);
-        navigator.addViewChangeListener(new MyViewChangeListener());
-
-        navigator.navigateTo(LoginView.VIEW_NAME);
-    }
-
-    /**
-     * The heart of the application's security. Every change of view is challenged here against the user's permissions.
-     */
-    private class MyViewChangeListener implements ViewChangeListener {
-
-        @Override
-        public boolean beforeViewChange(ViewChangeEvent event) {
-            // popups are not tied to the views, which is a shame, so we have to close them before moving on
-            UI.getCurrent().getWindows().forEach(Window::close);
-
-            boolean goingToLoginView = LoginView.VIEW_NAME.equals(event.getViewName());
-            boolean goingToAdminView = AdminView.VIEW_NAME.equals(event.getViewName());
-
-            if (goingToLoginView) {
-                // anyone can access the login view but doing so will log you out
-                session.logout();
-                return true;
-            } else if (session.isAdmin() || session.isLoggedIn() && !goingToAdminView) {
-                // an admin can go anywhere, so does a normal user except for the admin view
-                return true;
-            } else {
-                // any other attempt is forbidden and redirects to the login view
-                navigator.navigateTo(LoginView.VIEW_NAME);
-                return false;
-            }
-        }
-
-        @Override
-        public void afterViewChange(ViewChangeEvent event) {
-        }
-
+        setContent(new MVerticalLayout()
+                .add(new MPanel()
+                        .withUndefinedSize()
+                        .withContent(new MVerticalLayout()
+                                .add(new MLabel("123"))
+                                .add(new MButton("Like"))
+                                .alignAll(Alignment.MIDDLE_CENTER)))
+                .alignAll(Alignment.MIDDLE_CENTER));
     }
 
 }
